144239a18622c5720074cbb7948d839d8b5b7df5
Devesh.Suryawanshi <deveshsuryawashi940@gmail.com>
2022-10-23 19:49:26 +0530

    This is a Program to find a Even Character from String

---
+++ index.js
@@ -0,0 +1,8 @@
+//write a Program to print the Even Character from Given String
+
+let str = "Git Assingment 02";
+for(let i=0; i<str.length; i++){
+    if(i %2 == 1){
+        console.log(str[i]);
+    }
+}
